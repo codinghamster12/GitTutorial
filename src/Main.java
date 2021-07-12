e2c70f331053c8b718cfa162e45949f39d3738fd
TL\akrambis <bisma.akram97@hotmail.com>
2021-07-12 16:27:26 +0500

    commit changes

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,9 @@ public class Main {
         System.out.println("Number 1" + num1);
         System.out.println("Number 2" + num2);
         System.out.println("Multiplication"+multiply(num1, num2));
-<<<<<<< HEAD
         System.out.println("Summation"+add(num1, num2));
-=======
         System.out.println("Subtraction"+subtract(num1, num2));
 
->>>>>>> bisma-dev
 
     }
     public static int multiply(int a, int b){
