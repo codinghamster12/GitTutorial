997930feca07027772a1621ff2e1bba6842d8276
TL\akrambis <bisma.akram97@hotmail.com>
2021-07-12 16:17:47 +0500

    print summation

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Number 1" + num1);
         System.out.println("Number 2" + num2);
         System.out.println("Multiplication"+multiply(num1, num2));
+        System.out.println("Summation"+add(num1, num2));
 
     }
     public static int multiply(int a, int b){
