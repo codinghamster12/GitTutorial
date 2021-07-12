975dacec9a34c0b7341590df9d4f2017d43350e3
TL\akrambis <bisma.akram97@hotmail.com>
2021-07-12 15:19:34 +0500

    summation is implemented

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         return a * b;
     }
     public static int add(int a, int b){
-        return 0;
+        return a + b;
     }
     public static int subtract(int a, int b){
         return 0;
